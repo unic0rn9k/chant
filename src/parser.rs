4399a7fffb4eecad91e1b42f7bdf3e2a7b3faa1f
unic0rn9k <unic0rn9k@protonmail.com>
2022-06-19 15:25:42 +0200

    Implemented actual combinators

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,7 @@
 //! Parser combinator, implemented in rust, for the chant programming language
 
+use std::marker::PhantomData;
+
 use anyhow::*;
 
 const OPERATOR_CHARS: &str = ":=+-/*^&%|<>!";
@@ -16,6 +18,7 @@ pub enum Token {
     Operator(String),
     Separator(char),
     Blank,
+    Then(Box<Token>, Box<Token>),
 }
 
 impl Token {
@@ -32,14 +35,14 @@ impl Token {
 ///
 /// # Results
 /// The parser should return a token, and the index for the remainding (unparsed) part of the input string.
-pub trait SpecialParser {
+pub trait Combinator {
     fn parse(input: &str) -> Result<(Token, usize)>;
 }
 
 /// Parser for unsigned ints (list of digits)
 pub struct NaturalNumber;
 
-impl SpecialParser for NaturalNumber {
+impl Combinator for NaturalNumber {
     fn parse(i: &str) -> Result<(Token, usize)> {
         let mut num = 0;
         let mut rem = 0;
@@ -50,8 +53,8 @@ impl SpecialParser for NaturalNumber {
             }
             rem += 1;
         }
-        if rem == 0{
-            return Ok((Token::Blank, 0))
+        if rem == 0 {
+            return Ok((Token::Blank, 0));
         }
         Ok((Token::Number(num), rem))
     }
@@ -60,7 +63,7 @@ impl SpecialParser for NaturalNumber {
 /// Parser for any integer (list of digits, that might be pre-pended with '-')
 pub struct Integer;
 
-impl SpecialParser for Integer {
+impl Combinator for Integer {
     fn parse(i: &str) -> Result<(Token, usize)> {
         if i.chars().nth(0) == Some('-') {
             let mut n = NaturalNumber::parse(&i[1..])?;
@@ -75,27 +78,27 @@ impl SpecialParser for Integer {
 
 pub struct Symbol;
 
-impl SpecialParser for Symbol {
+impl Combinator for Symbol {
     fn parse(i: &str) -> Result<(Token, usize)> {
         let mut buffer = vec![];
         let mut i = i.chars();
 
         // check that first charecter is alphabetical og '_'
         let fc = i.next().unwrap() as u8;
-        if fc == 95 || (fc > 64 && fc < 91) || (fc > 96 && fc < 123){
+        if fc == 95 || (fc > 64 && fc < 91) || (fc > 96 && fc < 123) {
             buffer.push(fc as char)
-        }else{
-            return Ok((Token::Blank, 0))
+        } else {
+            return Ok((Token::Blank, 0));
         }
 
         // all other charecters can also be numbers...
         let mut rem = 1;
-        for c in i{
+        for c in i {
             let c = c as u8;
-            if c == 95 || (c > 64 && c < 91) || (c > 96 && c < 123) || (c > 47 && c < 58){
+            if c == 95 || (c > 64 && c < 91) || (c > 96 && c < 123) || (c > 47 && c < 58) {
                 buffer.push(c as char)
-            }else{
-                break
+            } else {
+                break;
             }
             rem += 1;
         }
@@ -106,22 +109,32 @@ impl SpecialParser for Symbol {
 
 pub struct Operator;
 
-impl SpecialParser for Operator {
+impl Combinator for Operator {
     fn parse(i: &str) -> Result<(Token, usize)> {
         let mut rem = 0;
 
-        for c in i.chars(){
-            if !OPERATOR_CHARS.contains(c){
-                break
+        for c in i.chars() {
+            if !OPERATOR_CHARS.contains(c) {
+                break;
             }
             rem += 1
         }
 
-        if rem == 0{
-            return Ok((Token::Blank, 0))
+        if rem == 0 {
+            return Ok((Token::Blank, 0));
         }
 
         Ok((Token::Operator((&i[0..rem]).to_string()), rem))
+    }
+}
+
+pub struct Then<A: Combinator, B: Combinator>(PhantomData<A>, PhantomData<B>);
+
+impl<A: Combinator, B: Combinator> Combinator for Then<A, B> {
+    fn parse(i: &str) -> Result<(Token, usize)> {
+        let a = A::parse(i)?;
+        let b = B::parse(&i[a.1..])?;
+        Ok((Token::Then(Box::new(a.0), Box::new(b.0)), a.1 + b.1))
     }
 }
 
@@ -130,11 +143,8 @@ mod tests {
     use crate::parser::*;
 
     #[test]
-    fn int() -> Result<()>{
-        assert_eq!(
-            NaturalNumber::parse("123")?,
-            (Token::Number(123), 3)
-        );
+    fn int() -> Result<()> {
+        assert_eq!(NaturalNumber::parse("123")?, (Token::Number(123), 3));
         assert_eq!(NaturalNumber::parse("-123")?.0, Token::Blank);
         assert_eq!(Integer::parse("-123")?, (Token::Number(-123), 4));
         assert_eq!(Integer::parse("123")?, (Token::Number(123), 3));
@@ -143,15 +153,37 @@ mod tests {
     }
 
     #[test]
-    fn symbol() -> Result<()>{
-        assert_eq!(Symbol::parse("_oki123")?, (Token::Symbol("_oki123".to_string()), 7));
+    fn symbol() -> Result<()> {
+        assert_eq!(
+            Symbol::parse("_oki123")?,
+            (Token::Symbol("_oki123".to_string()), 7)
+        );
         assert_eq!(Symbol::parse("1_oki123")?.0, Token::Blank);
         Ok(())
     }
 
     #[test]
-    fn op() -> Result<()>{
-        assert_eq!(Operator::parse("+=")?, (Token::Operator("+=".to_string()), 2));
+    fn op() -> Result<()> {
+        assert_eq!(
+            Operator::parse("+=")?,
+            (Token::Operator("+=".to_string()), 2)
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn symbol_then_num() -> Result<()> {
+        assert_eq!(
+            Then::<Integer, Symbol>::parse("123abc")?,
+            (
+                Token::Then(
+                    Box::new(Token::Number(123)),
+                    Box::new(Token::Symbol("abc".to_string())),
+                ),
+                6
+            )
+        );
+
         Ok(())
     }
 }
